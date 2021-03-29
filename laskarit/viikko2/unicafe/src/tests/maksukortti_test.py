5fd80fd078cdba5653f5ded092255384aea7f6b8
Kia Lehti <kia.lehti@gmail.com>
2021-03-29 20:58:34 +0300

    maksukorttitestit

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,28 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")
+
+    def test_saldo_vahenee_oikein_jos_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(3)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.07")
+    
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(3)
+        self.maksukortti.ota_rahaa(3)
+        self.maksukortti.ota_rahaa(3)
+        self.maksukortti.ota_rahaa(3)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.01")
+    
+    def test_metodi_palauttaa_true_jos_riittaa(self):
+        self.maksukortti.ota_rahaa(9)
+        self.assertTrue(str(self.maksukortti), True)
+    def test_metodi_palauttaa_false_jos_ei_riitta(self):
+        self.maksukortti.ota_rahaa(11)
+        self.assertTrue(str(self.maksukortti), False)
